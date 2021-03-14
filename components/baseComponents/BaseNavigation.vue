dae765e1f182cc0f81111da5986264abeea5f26b
Sandeep-777 <sandeepsthapit@gmail.com>
2021-03-14 19:50:16 +0545

    modules dropdown added

--- components/baseComponents/BaseNavigation.vue
+++ components/baseComponents/BaseNavigation.vue
@@ -27,15 +27,23 @@
 
     <div id="navigationMenu" class="navbar-menu" ref="navMenu">
       <div class="navbar-end">
-        <nuxt-link
-          :to="{ name: 'index___' + this.$i18n.locale }"
-          class="navbar-item"
-          >Modules</nuxt-link
-        >
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link"> {{ $t("modules") }} </a>
+
+          <div class="navbar-dropdown">
+            <nuxt-link
+              v-for="lesson in lessons"
+              :key="lesson.title"
+              :to="lesson.link"
+              class="navbar-item"
+              >{{ lesson.title }}</nuxt-link
+            >
+          </div>
+        </div>
         <nuxt-link
           :to="{ name: 'glossaryGlobal___' + this.$i18n.locale }"
           class="navbar-item"
-          >Glossary</nuxt-link
+          >{{ $t("glossary") }}</nuxt-link
         >
         <GlossaryLangSwitch class="navbar-item" />
         <LangSwitch class="navbar-item" />
@@ -51,6 +59,36 @@ export default {
   components: {
     LangSwitch,
     GlossaryLangSwitch,
+  },
+  computed: {
+    lessons() {
+      return [
+        {
+          title: this.$t("lesson1"),
+          link: { name: "pollination___" + this.$i18n.locale },
+        },
+        {
+          title: this.$t("lesson2"),
+          link: { name: "pollination___" + this.$i18n.locale },
+        },
+        {
+          title: this.$t("lesson3"),
+          link: { name: "pollination___" + this.$i18n.locale },
+        },
+        {
+          title: this.$t("lesson4"),
+          link: { name: "pollination___" + this.$i18n.locale },
+        },
+        {
+          title: this.$t("lesson5"),
+          link: { name: "pollination___" + this.$i18n.locale },
+        },
+        {
+          title: this.$t("lesson6"),
+          link: { name: "pollination___" + this.$i18n.locale },
+        },
+      ];
+    },
   },
   methods: {
     navBurger() {
