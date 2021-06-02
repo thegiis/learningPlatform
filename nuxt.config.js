a6fe86f7b8840dcd63dc8a63aae955835376cad0
rushilshakya <rushil.shakya@gmail.com>
2021-06-02 15:00:39 +0545

    feat: configure base directory

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,8 +16,14 @@ export default {
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" },
-      { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css" },
+      {
+        rel: "stylesheet",
+        href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css",
+      },
     ],
   },
 
@@ -64,5 +70,8 @@ export default {
   build: {},
   generate: {
     fallback: "404.html",
+  },
+  router: {
+    base: "/ncp",
   },
 };
