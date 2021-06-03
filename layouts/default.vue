022261b847cc19eb2425497b819320d46a154ab5
rushilshakya <rushil.shakya@gmail.com>
2021-06-03 20:59:53 +0545

    fix: cookiz.get behavior different in static vs dev

--- layouts/default.vue
+++ layouts/default.vue
@@ -41,8 +41,8 @@ export default {
     };
   },
   created() {
-    const storedLocale = this.$cookiz.get("lang");
-    const storedGlossaryLocale = this.$cookiz.get("glossaryLang");
+    const storedLocale = this.$cookiz.get("lang") || undefined;
+    const storedGlossaryLocale = this.$cookiz.get("glossaryLang") || undefined;
 
     if (storedGlossaryLocale !== undefined) {
       this.$store.dispatch("lesson/setVocabLocale", storedGlossaryLocale);
@@ -118,7 +118,7 @@ export default {
     width: 0px;
   }
 }
-.contentWrapper{
+.contentWrapper {
   position: relative;
   margin: 0 auto;
   width: 100%;
