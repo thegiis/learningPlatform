0651c3e839b2b5a053e3600c3b82a3d1582640f7
Sandeep-777 <sandeepsthapit@gmail.com>
2021-06-15 13:21:19 +0545

    fixed multiple vocab error bug

--- components/baseComponents/BaseText.vue
+++ components/baseComponents/BaseText.vue
@@ -5,9 +5,9 @@
         v-if="word.hasVocab"
         class="default-vocab-word"
         :class="getClass(word.isHighlight)"
-        @mouseover="onOver(word.vocab, $event)"
+        @mouseover="onOver(word.vocab, index)"
         @mouseleave="onLeave()"
-        ref="vocabItem"
+        :ref="'vocabItem-'+index"
         v-bind:key="index"
         >{{ word.text }}
       </span>
@@ -89,14 +89,16 @@ export default {
         const selectors = this.$store.state.lesson.selectors;
         this.words.forEach(function (word) {
           let newWord = [word];
+          let hasMultipleVocab = false;
           selectors.forEach(function (selector) {
             // find the selector in string
             const textData = word["text"];
             const temp = textData.toLowerCase();
-            const idx = temp.indexOf(selector);
+            let idx = temp.indexOf(selector);
+            // console.log(selector, idx);
             if (idx != -1) {
               // check last character if it is punctuation or some other alphanumeric
-              const lastIdx = idx + selector.length;
+              let lastIdx = idx + selector.length;
               const lastChar = temp[lastIdx];
               const isNotWord = !isAlphaNumeric(lastChar);
               if (isNotWord) {
@@ -105,17 +107,38 @@ export default {
                 let vocabWord = { ...word };
                 let lastWord = { ...word };
 
-                const firstPart = textData.slice(0, idx);
-                const vocabPart = textData.slice(idx, lastIdx);
-                const lastPart = textData.slice(lastIdx);
+                // check if there are multiple vocabs
+                if (hasMultipleVocab) {
+                  const remText = newWord[newWord.length - 1]["text"];
+                  idx = remText.indexOf(selector);
+                  lastIdx = idx + selector.length;
+                  const firstPart = remText.slice(0, idx);
+                  const vocabPart = remText.slice(idx, lastIdx);
+                  const lastPart = remText.slice(lastIdx);
 
-                firstWord["text"] = firstPart;
-                vocabWord["text"] = vocabPart;
-                vocabWord["hasVocab"] = true;
-                vocabWord["vocab"] = tempVocab[selector];
-                lastWord["text"] = lastPart;
+                  firstWord["text"] = firstPart;
+                  vocabWord["text"] = vocabPart;
+                  vocabWord["hasVocab"] = true;
+                  vocabWord["vocab"] = tempVocab[selector];
+                  lastWord["text"] = lastPart;
 
-                newWord = [firstWord, vocabWord, lastWord];
+                  newWord.pop();
+                  newWord = newWord.concat([firstWord, vocabWord, lastWord]);
+                } else {
+                  const firstPart = textData.slice(0, idx);
+                  const vocabPart = textData.slice(idx, lastIdx);
+                  const lastPart = textData.slice(lastIdx);
+
+                  firstWord["text"] = firstPart;
+                  vocabWord["text"] = vocabPart;
+                  vocabWord["hasVocab"] = true;
+                  vocabWord["vocab"] = tempVocab[selector];
+                  lastWord["text"] = lastPart;
+
+                  newWord = [firstWord, vocabWord, lastWord];
+                  hasMultipleVocab = true;
+                }
+                // console.log(newWord);
               }
             }
           });
@@ -134,9 +157,10 @@ export default {
     },
 
     //hover function
-    onOver(data, event) {
+    onOver(data, index) {
       // get word position and decide where vocab card shows up
-      const vocabItem = this.$refs.vocabItem[0];
+      const vocabItem = this.$refs['vocabItem-'+index][0];
+      console.log(vocabItem)
       const rect = vocabItem.getBoundingClientRect();
       this.$store.dispatch("lesson/setVocab");
       this.$store.dispatch("lesson/setCurrentWord", data);
