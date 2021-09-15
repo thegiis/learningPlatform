a6bcd4b0743be93116493b62e5054dd817411c08
rushilshakya <rushil.shakya@gmail.com>
2021-09-15 21:40:59 +0545

    style: Adjust page heading spacing

--- components/lessonComponents/LessonIndex.vue
+++ components/lessonComponents/LessonIndex.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <div class="wrapper" data-variant="more-padding">
+    <div
+      class="wrapper"
+      :class="$style.bannerSpacing"
+      data-variant="more-padding"
+    >
       <h1 class="lesson">
         {{ currentLessonLang.currentPage.type.toUpperCase() }}
       </h1>
-      <h4 class="lesson">Nature's contributions to people</h4>
+      <h4 class="lesson" :class="$style.h4">
+        Nature's contributions to people
+      </h4>
       <h3 class="lesson">{{ currentLessonLang.title.toUpperCase() }}</h3>
     </div>
     <slot></slot>
@@ -21,3 +27,12 @@ export default {
   },
 };
 </script>
+<style module>
+.h4 {
+  margin-top: 8px;
+  margin-bottom: 4px;
+}
+.bannerSpacing {
+  margin-bottom: 40px;
+}
+</style>
