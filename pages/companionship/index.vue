9c58bc64e661f92dcaf7837717ba9dfd1e77dcf4
rushilshakya <rushil.shakya@gmail.com>
2021-09-18 20:15:21 +0545

    feat: Add more summary coming soons

---
+++ pages/companionship/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <LessonIndexSummary
+    summaryImage="/img/companionship/dogAndPerson.jpg"
+    summary="Companionship and blah blah.  Companionship and blah blah.  Companionship and blah blah.  Companionship and blah blah.  Companionship and blah blah.  Companionship and blah blah.  "
+  />
+</template>
