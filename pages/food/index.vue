2d8b9f75567ffe4549bf18d5914b45c50f691501
rushilshakya <rushil.shakya@gmail.com>
2021-09-19 23:05:58 +0545

    feat: Food summary

--- pages/food/index.vue
+++ pages/food/index.vue
@@ -1,6 +1,6 @@
 <template>
   <LessonIndexSummary
     summaryImage="/img/food/familyEating.jpg"
-    summary="Food and blah blah.  Food and blah blah.  Food and blah blah.  Food and blah blah.  Food and blah blah.  Food and blah blah.  "
+    summary="Nature provides conditions and direct contributions for food production.  Globally food production has been increasing but unsustainable food production has negatively impacted Nature causing environmental pollution and biodiversity loss.  We are producing more food than we require hence majority of food produced is wasted.  To ensure food supply and preserve nature, some of the things we can do are: change our dietary habits, reduce food waste and consume locally produce foods."
   />
 </template>
