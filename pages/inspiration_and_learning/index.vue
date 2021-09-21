cafa14db922effea656d301e2678f647ee5a7ed3
rushilshakya <rushil.shakya@gmail.com>
2021-09-21 09:47:08 +0545

    chore: Change summary text

--- pages/inspiration_and_learning/index.vue
+++ pages/inspiration_and_learning/index.vue
@@ -1,6 +1,6 @@
 <template>
   <LessonIndexSummary
     summaryImage="/img/inspiration_and_learning/baanuWriting.jpg"
-    summary="Inspiration and learning and blah blah.  Inspiration and learning and blah blah.  Inspiration and learning and blah blah.  Inspiration and learning and blah blah.  Inspiration and learning and blah blah.  Inspiration and learning and blah blah.  "
+    summary="Inspiration and learning"
   />
 </template>
